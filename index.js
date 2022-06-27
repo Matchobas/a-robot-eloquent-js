566b0ced1e529457a7b29944bd3746daaec4c189
Matchobas <maalexandria@hotmail.com>
2022-06-26 23:47:12 -0300

    Testing the VillageState class

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
 import { VillageState } from './VillageState.js';
 
-const villageState = new VillageState('Post Office', [{place: "Post Office", address: "Alice's House"}]);
+const villageState = new VillageState('Post Office', [
+  {place: "Post Office", address: "Alice's House"},
+  {place: "Alice's House", address: "Shop"},
+  {place: "Shop", address: "Town Hall"},
+  {place: "Post Office", address: "Farm"},
+  {place: "Farm", address: "Alice's House"},
+]);
 
 let next = villageState.move("Alice's House");
 console.log(next.place);
+console.log(next.parcels);
+
+next = next.move("Bob's House");
+console.log(next.place);
+console.log(next.parcels);
+
+next = next.move("Town Hall");
+console.log(next.place);
+console.log(next.parcels);
+
+next = next.move("Shop");
+console.log(next.place);
+console.log(next.parcels);
+
+next = next.move("Town Hall");
+console.log(next.place);
+console.log(next.parcels);
+
+next = next.move("Shop");
+console.log(next.place);
+console.log(next.parcels);
+
+next = next.move("Marketplace");
+console.log(next.place);
+console.log(next.parcels);
+
+next = next.move("Farm");
+console.log(next.place);
+console.log(next.parcels);
+
+next = next.move("Marketplace");
+console.log(next.place);
+console.log(next.parcels);
+
+next = next.move("Post Office");
+console.log(next.place);
+console.log(next.parcels);
+
+next = next.move("Alice's House");
+console.log(next.place);
+console.log(next.parcels);
